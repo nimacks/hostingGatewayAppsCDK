2c1725b5883f12cb025ff10f6ac00737910c49e6
Kevin Old <kevin@kevinold.com>
2023-02-01 14:29:55 -0600

    get amplify app stack deploying

--- lib/hosting_gateway_apps_cdk-stack.ts
+++ lib/hosting_gateway_apps_cdk-stack.ts
@@ -1,16 +1,48 @@
+import * as aws_amplify from "@aws-cdk/aws-amplify-alpha";
 import * as cdk from 'aws-cdk-lib';
+import { SecretValue } from "aws-cdk-lib";
+import * as aws_codebuild from "aws-cdk-lib/aws-codebuild";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HostingGatewayAppsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const initials = "TEST"
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HostingGatewayAppsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const amplifyApp = new aws_amplify.App(this, `${initials}-amplify-app`, {
+      sourceCodeProvider: new aws_amplify.GitHubSourceCodeProvider({
+        repository: "202301091107-amplify-cli-test-plan-spa-cra",
+        oauthToken: SecretValue.secretsManager("kold-github-token"),
+        owner: "kevinold",
+      }),
+      buildSpec: aws_codebuild.BuildSpec.fromObject({
+        version: 1,
+        frontend: {
+          phases: {
+            preBuild: {
+              commands: [
+                "npm ci",
+              ],
+            },
+            build: {
+              commands: ["npm run build"],
+            },
+          },
+          artifacts: {
+            baseDirectory: "build",
+            files: ["**/*"],
+          },
+        },
+      }),
+    });
+
+    amplifyApp.addBranch("main");
+
+    amplifyApp.addCustomRule(
+      aws_amplify.CustomRule.SINGLE_PAGE_APPLICATION_REDIRECT
+    );
+
+    //amplifyApp.addEnvironment("REACT_APP_BASE_API_URL", "test"); 
   }
 }
