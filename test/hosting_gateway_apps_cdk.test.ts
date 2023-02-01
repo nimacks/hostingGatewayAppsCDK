8d843725754e393e0d523e6ae10ff3efc97cd1ad
Kevin Old <kevin@kevinold.com>
2023-02-01 11:42:17 -0600

    Initial commit

---
+++ test/hosting_gateway_apps_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HostingGatewayAppsCdk from '../lib/hosting_gateway_apps_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hosting_gateway_apps_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HostingGatewayAppsCdk.HostingGatewayAppsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
