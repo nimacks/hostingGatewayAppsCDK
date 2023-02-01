1ed3b261ed608b4570ef93af15a09a78eb603d04
Kevin Old <kevin@kevinold.com>
2023-02-01 15:45:05 -0600

    lock to region

--- bin/hosting_gateway_apps_cdk.ts
+++ bin/hosting_gateway_apps_cdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { HostingGatewayAppsCdkStack } from '../lib/hosting_gateway_apps_cdk-stack';
 
 const app = new cdk.App();
@@ -12,6 +12,7 @@ new HostingGatewayAppsCdkStack(app, 'HostingGatewayAppsCdkStack', {
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-2' },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
