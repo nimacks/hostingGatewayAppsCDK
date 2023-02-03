c06cc54d59816119c12b0093c1b08762e1f2ecc0
Kevin Old <kevin@kevinold.com>
2023-02-03 16:03:39 -0600

    cleanup

--- transforms.js
+++ transforms.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
 const lodash = require("lodash");
 const path = require("path");
-//const { fileURLToPath } = require("url");
 const apps = require("./apps.json");
 const CDKOutput = require("./infra/hostingGatewayAppsCDK/cdk-outputs.json");
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
 
 const { find, forEach } = lodash;
 let appsWithIds = [];
