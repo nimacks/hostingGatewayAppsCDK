2d9029ab2b21003562dff2d005ab31289d6fa19c
Kevin Old <kevin@kevinold.com>
2023-02-07 19:40:48 -0600

    wip - iterate on update apps json script

--- updateAppsJsonWithAppIds.js
+++ updateAppsJsonWithAppIds.js
@@ -1,24 +1,50 @@
-const fs = require("fs");
-const lodash = require("lodash");
-const path = require("path");
+//const fs = require("fs");
+const lodash = require("lodash/fp");
+//const path = require("path");
 const apps = require("./apps.json");
 const CDKOutput = require("./infra/hostingGatewayAppsCDK/cdk-outputs.json");
 
-const { find, forEach } = lodash;
-let appsWithIds = [];
+const { identity, map, pipe, tap, flatten } = lodash;
+const debug = tap(console.log);
 
-forEach(CDKOutput["HostingGatewayAppsCdkStack"], function (app) {
-  const [name, id] = app.split("|");
-  console.log(name, id);
+const res = pipe(
+  map((a) => map(identity, a)),
+  flatten
+  //debug
+)(CDKOutput);
 
-  const appRecord = find(apps, (a) => a.name === name);
-  // @ts-ignore
-  appRecord["id"] = id;
-  appsWithIds.push(appRecord);
-  console.log(appsWithIds);
-});
+console.log(res);
 
-fs.writeFileSync(
-  path.join(__dirname, "apps.json"),
-  JSON.stringify(appsWithIds, null, 2)
-);
+// let appsWithIds = {};
+
+// mapValues(CDKOutput, function (stack) {
+//   //console.log(stack);
+//   mapValues(stack, function (app) {
+//     const [name, id] = app.split("|");
+//     console.log(app);
+//     appsWithIds[name] = id;
+//   });
+// });
+
+// console.log(appsWithIds);
+
+// each(CDKOutput, function (stack) {
+//   //console.log("stack", stack);
+//   forEach(stack, function (app) {
+//     const [name, id] = app.split("|");
+//     console.log(name, id);
+
+//     appsWithIds[name].push(id);
+//     //const appRecord = find(apps, (a) => a.name === name);
+//     // @ts-ignore
+//     //appRecord["id"] = id;
+//     //appsWithIds.push(appRecord);
+//     //console.log(appsWithIds);
+//   });
+// });
+// console.log(appsWithIds);
+
+// fs.writeFileSync(
+//   path.join(__dirname, "apps.json"),
+//   JSON.stringify(appsWithIds, null, 2)
+// );
